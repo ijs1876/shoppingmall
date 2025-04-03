68847172a2c163ced8c270fd248934b77d6a6ff8
User <User@host.docker.internal>
2025-04-03 16:25:00 +0900

    aa

--- demo-1/src/main/java/com/example/shoppingmall/Demo1Application.java
+++ demo-1/src/main/java/com/example/shoppingmall/Demo1Application.java
@@ -3,6 +3,7 @@ package com.example.shoppingmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Demo1Application {
 
